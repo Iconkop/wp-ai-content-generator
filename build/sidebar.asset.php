4ef9e8a11e685110958cd86f48705b68d63ce152
iconkop <shenzhan22@gmail.com>
2026-05-06 12:35:14 +0800

    ci: Optimize the build script cache and change the trigger mode.

---
+++ build/sidebar.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '5ca4da675a9e12a34852');
